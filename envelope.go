231f02a8c953c7429751380f0438c1794dc99be4
J Plasmeier <justplaz@amazon.com>
2023-10-10 14:32:29 -0700

    remove unused cipher_util and test, remove tests from cmm test

--- envelope.go
+++ envelope.go
@@ -1,6 +1,7 @@
 package s3crypto
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"strconv"
@@ -95,4 +96,26 @@ func getJSONNumberAsString(data []byte) (string, error) {
 	}
 
 	return "", fmt.Errorf("failed to parse as JSON Number")
+}
+
+func encodeMeta(reader lengthReader, cd CryptographicMaterials) (ObjectMetadata, error) {
+	iv := base64.StdEncoding.EncodeToString(cd.IV)
+	key := base64.StdEncoding.EncodeToString(cd.EncryptedKey)
+
+	contentLength := reader.GetContentLength()
+
+	matdesc, err := cd.MaterialDescription.encodeDescription()
+	if err != nil {
+		return ObjectMetadata{}, err
+	}
+
+	return ObjectMetadata{
+		CipherKey:             key,
+		IV:                    iv,
+		MatDesc:               string(matdesc),
+		KeyringAlg:            cd.KeyringAlgorithm,
+		CEKAlg:                cd.CEKAlgorithm,
+		TagLen:                cd.TagLength,
+		UnencryptedContentLen: strconv.FormatInt(contentLength, 10),
+	}, nil
 }
