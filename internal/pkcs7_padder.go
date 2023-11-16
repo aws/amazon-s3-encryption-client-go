10f3377e70643cb8d4775bb21a684e7ec80182f0
Jose Corella <joseeduardocorella@gmail.com>
2023-11-15 16:57:43 -0800

    chore: add license header to files

--- internal/pkcs7_padder.go
+++ internal/pkcs7_padder.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package internal
 
 // Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
